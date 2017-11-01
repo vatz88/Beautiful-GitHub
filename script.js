da8c52de1ce12b21629e90fffe4db93253f50a8c
Vatsal Joshi <vatz88@gmail.com>
2017-11-01 09:40:34 +0530

    :fire: bad colors commented

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ var all_new_colors = [
     // Blue
     ["#82B1FF", "#448AFF", "#2979FF", "#2962FF"],
     // Light Blue
-    ["#80D8FF", "#40C4FF", "#00B0FF", "#0091EA"],
+    // ["#80D8FF", "#40C4FF", "#00B0FF", "#0091EA"],
     // Cyan
-    ["#84FFFF", "#18FFFF", "#00E5FF", "#00B8D4"],
+    // ["#84FFFF", "#18FFFF", "#00E5FF", "#00B8D4"],
     // Teal
-    ["#A7FFEB", "#64FFDA", "#1DE9B6", "#00BFA5"],
+    // ["#A7FFEB", "#64FFDA", "#1DE9B6", "#00BFA5"],
     // Green
     ["#B9F6CA", "#69F0AE", "#00E676", "#00C853"],
     // Light Green
-    ["#CCFF90", "#B2FF59", "#76FF03", "#64DD17"],
+    // ["#CCFF90", "#B2FF59", "#76FF03", "#64DD17"],
     // Lime
-    ["#F4FF81", "#EEFF41", "#C6FF00", "#AEEA00"],
+    // ["#F4FF81", "#EEFF41", "#C6FF00", "#AEEA00"],
     // Yellow
-    ["#FFFF8D", "#FFFF00", "#FFEA00", "#FFD600"],
+    // ["#FFFF8D", "#FFFF00", "#FFEA00", "#FFD600"],
     // Amber
     ["#FFE57F", "#FFD740", "#FFC400", "#FFAB00"],
     // Orange
-    ["#FFD180", "#FFAB40", "#FF9100", "#FF6D00"],
+    // ["#FFD180", "#FFAB40", "#FF9100", "#FF6D00"],
     // Deep Orange
-    ["#FF9E80", "#FF6E40", "#FF3D00", "#DD2C00"],
+    ["#FF9E80", "#FF6E40", "#FF3D00", "#DD2C00"]
 ];
 
 var new_color = all_new_colors[Math.floor(Math.random() * all_new_colors.length)];
@@ -39,14 +39,14 @@ var old_color = (function () {
     var arr = [];
     $("ul.legend").children("li").not(":first").each(function (index) {
         var color = rgb2hex($(this).css("background-color"));
-        arr.push(color.toLocaleUpperCase());
+        arr.push(color.toUpperCase());
         $(this).css("background-color", new_color[index]);
     });
     return arr;
 })();
 
-$("rect").each(function () {
-    switch ($(this).attr("fill")) {
+$("rect.day").each(function () {
+    switch ($(this).attr("fill").toUpperCase()) {
         case old_color[0]:
             $(this).attr("fill", new_color[0]);
             break;
